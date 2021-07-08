c9fc72087f597f8a99fb34851f173598c4442090
Martin Najjar <martin_najjar@yahoo.com>
2021-07-09 01:19:49 +0300

    Fix linter issues

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -1,18 +1,18 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { connectToDatabase } from '../../util/mongodb';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { connectToDatabase } from '../../util/mongodb'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    const data = req.body;
+    const data = req.body
 
-    const { db } = await connectToDatabase();
+    const { db } = await connectToDatabase()
 
-    const strangeoriesCollection = db.collection('strangeories');
+    const strangeoriesCollection = db.collection('strangeories')
 
-    const result = await strangeoriesCollection.insertOne(data);
+    const result = await strangeoriesCollection.insertOne(data)
 
-    res.status(201).json({ message: 'Story was inserted!' });
+    res.status(201).json({ message: 'Story was inserted!' })
   }
-};
+}
 
-export default handler;
+export default handler
