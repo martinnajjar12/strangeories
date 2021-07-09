b7725c9872f83d762862bb867a5b008f3b374d2a
Martin Najjar <martin_najjar@yahoo.com>
2021-07-09 21:17:35 +0300

    Fix linter issues

--- theme/theme.ts
+++ theme/theme.ts
@@ -1,12 +1,12 @@
-import { createTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles'
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
     primary: {
       main: '#3f51b5',
-    },
-  },
-});
+    }
+  }
+})
 
-export default theme;
+export default theme
