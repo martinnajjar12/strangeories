bac65ba89534df758207a15621633221ccb156ac
Martin Najjar <martin_najjar@yahoo.com>
2021-08-31 00:01:18 +0300

    Fix Node.js CI error

--- __tests__/newStoryPage.tsx
+++ __tests__/newStoryPage.tsx
@@ -4,6 +4,6 @@ import { render, screen } from '@testing-library/react';
 describe('New Story Page', () => {
   it('should render CREATE STORY on the document', () => {
     render(<Form />);
-    expect(screen.getByRole('button')).toHaveTextContent('Create StorY');
+    expect(screen.getByRole('button')).toHaveTextContent('Create Story');
   });
 });
